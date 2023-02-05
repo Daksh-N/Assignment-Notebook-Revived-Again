d7513aa1677ab5fcc1ca19779d4a47bbaa802bdd
Daksh <dnakra@bsd220.org>
2023-02-05 12:20:16 -0600

    Initial Commit

---
+++ Assignment Notebook Revived Again/Assignment_Notebook_Revived_AgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Notebook_Revived_AgainApp.swift
+//  Assignment Notebook Revived Again
+//
+//  Created by Daksh Nakra on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Notebook_Revived_AgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
